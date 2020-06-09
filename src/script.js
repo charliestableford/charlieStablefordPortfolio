e644c6398cf4e9a1f8323c5c29e03d4a216c95a0
Charlie Theodore <charliestableford2@gmail.com>
2020-06-08 17:53:24 -0700

    more js

--- src/script.js
+++ src/script.js
@@ -2,25 +2,59 @@ console.log("main works");
 
 const hero = document.querySelector('.description');
 const text = hero.querySelector('.bold');
+// how many pixels at it most should it be stretched
 const movement = 20; 
 
-function shadow(e) {
-  const { offsetWidth: width, offsetHeight: height } = hero;
-  let { offsetX: x, offsetY: y } = e;
+function shadow(e){
+    const width = hero.offsetWidth;
+    const height = hero.offsetHeight;
 
-  if (this !== e.target) {
-    x = x + e.target.offsetLeft;
-    y = y + e.target.offsetTop;
-  }
+    // get the info about where the cursor is
+    // this is only for the hero - think about making it for the body?
+    // normalization - if we are hovering over the child make it consitent.
+    let x = e.offsetX;
+    let y = e.offsetY;
 
-  const xWalk = Math.round((x / width * movement) - (movement / 2));
-  const yWalk = Math.round((y / height * movement) - (movement / 2));
+    // this is the thing you are listening on.
+    console.log(this);
 
-  text.style.textShadow = `
-    ${xWalk}px ${yWalk}px 0 rgba(42,42,42,.2)
- 
-  `;
+    if(this !== e.target){
+        // if these are not the same them 
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
 
+    //Math.round to make it shorter and confirm the above equation
+    const xMove = Math.round((x / width * movement) - (movement /2));
+    const yMove = Math.round((y / height * movement) - (movement /2));
+
+    // sub in the above coordinates
+    text.style.textShadow = `${xMove}px ${yMove}px 0 rgba(241, 59, 57, 0.75) `;
+
+    // console.log(xMove, yMove);
+    // console.log(x,y);
 }
 
 hero.addEventListener('mousemove', shadow);
+
+
+// //parallax
+// const abstractOne = document.querySelector('.abstractOne');
+// const abstractTwo = document.querySelector('.abstractTwo');
+// const abstractThree = document.querySelector('.abstractThree');
+// const abstractFour = document.querySelector('.abstractFour');
+// const abstractFive = document.querySelector('.abstractFive');
+// const abstractSix = document.querySelector('.abstractSix');
+
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+//     console.log(value);
+
+//     // abstractOne.style.top= value * 0.75+ 'px';
+//     // abstractTwo.style.top= value * 1.5+ 'px';
+//     // abstractThree.style.top= value * 0.15+ 'px';
+//     // abstractFour.style.top= value * 0.5+ 'px';
+//     // abstractFive.style.top= value * 0.8+ 'px';
+//     abstractSix.style.top=value * 1.2 + 'px';
+//     abstractSix.style.right=value*0.08 +'px';
+// })
