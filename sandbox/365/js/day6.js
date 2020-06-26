6922f9ce59d5e43e02a76ff9930f5e1e4e3897e1
Charlie Theodore <charliestableford2@gmail.com>
2020-06-26 09:52:08 -0700

    progress

--- sandbox/365/js/day6.js
+++ sandbox/365/js/day6.js
@@ -3,10 +3,40 @@ const miniPic = document.querySelectorAll('img');
 console.log(miniPic);
 const bigPic = document.querySelector('.bigPic');
 
-function handleClick(){
-    console.log('you clicked');
-    bigPic.style.display = 'block';
-}
+function handleClick(event){
+        console.log(event.currentTarget);
+        //when somebody clicks on a tab we have to hide the other tab panels
+        // console.log(tabPanels);
+        //loop over with foreach
+        // tabPanels.forEach(function(panel){
+            // console.log(panel);
+            //take the panel element
+            //hide them all
+            // panel.hidden = true;
+        // });
+        //mark all tabs as inactive.
+        // tabButtons.forEach(tab => {
+            //taking the tab
+            // tab.ariaSelected = false;
+            // tab.setAttribute('aria-selected', false);
+        // })
+        //mark the clicked tab as selected
+        //accessiblity attribute over classes.
+        // event.currentTarget.setAttribute('aria-selected', true)
+    // ;    // find the associated tab panel and show it
+        // const { id } = event.currentTarget;
+        // console.log(id);
+        //dynamic selector
+        // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+        // console.log()
+        // tabPanel.hidden = false;
+    }
+
+    // console.log(`you clicked ${miniPic.id}`);
+    // add style to each clicked.
+    // bigPic.style.display = 'block';
+    
+// }
 
 // loop over pics and add a click even to each
 miniPic.forEach(pic => pic.addEventListener('click', handleClick));
