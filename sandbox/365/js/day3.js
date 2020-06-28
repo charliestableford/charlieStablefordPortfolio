d48e080488ac966d122e8b5adce274ea9e8bf9c7
Charlie Theodore <charliestableford2@gmail.com>
2020-06-28 16:23:27 -0700

    working on minutes

--- sandbox/365/js/day3.js
+++ sandbox/365/js/day3.js
@@ -1,21 +1,26 @@
 const submit = document.querySelector('.submit');
 const inputHour = document.querySelector('.inputHour');
 const inputMinute = document.querySelector('.inputMinute');
+const alarmSound = document.querySelector('#alarmSound');
+// console.log(alarmSound);
 
 let date = new Date();
 let min = date.getMinutes();
 let hour = date.getHours();
 
-submit.addEventListener('click', async function(e){
+function alarming(e){
     e.preventDefault();
 
     //current hour and min
     console.log(hour, min);
     console.log(inputHour.value, inputMinute.value);
     //if hour and minutes is equal to each other play audio file
+     
     if(inputHour.value === hour && inputMinute.value === min){
-        // console.log(alarmSound);
+       console.log(alarmSound);
         //play audio
-        return alarmSound.play();
+        // return alarmSound.play();
     }
-});
+}
+
+submit.addEventListener('click', alarming);
