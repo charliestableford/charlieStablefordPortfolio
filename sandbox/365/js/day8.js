64967aea8793ba485f4cfd85fdb504b3c3f59144
Charlie Theodore <charliestableford2@gmail.com>
2020-06-27 20:06:47 -0700

    part of 8

---
+++ sandbox/365/js/day8.js
@@ -0,0 +1,25 @@
+console.log('day eight');
+
+const before = document.querySelector('.before');
+const after = document.querySelector('.after');
+const add = document.querySelector('.add');
+const subtract = document.querySelector('.subtract');
+
+const arr = ['🍏', '🍇', '🍅', '🍑', '🍌', '🍉', '🍊', '🥭'];
+
+function adding(arr){
+    console.log('added');
+    arr.push();
+
+}
+
+function subtracting(){
+    console.log('subtract');
+}
+
+add.addEventListener('click', adding);
+subtract.addEventListener('click', subtracting);
+
+
+
+
