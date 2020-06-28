2dd14510beedb8fa5e280c7d62c3ef2df85bf54e
Charlie Theodore <charliestableford2@gmail.com>
2020-06-28 13:04:08 -0700

    arrays day one

--- sandbox/365/js/day8.js
+++ sandbox/365/js/day8.js
@@ -6,19 +6,59 @@ const add = document.querySelector('.add');
 const subtract = document.querySelector('.subtract');
 
 const arr = ['🍏', '🍇', '🍅', '🍑', '🍌', '🍉', '🍊', '🥭'];
+const copy = [];
 
-function adding(arr){
-    console.log('added');
-    arr.push();
+function displayArr(){
+
+    let ul = document.createElement('ul');
+    before.appendChild(ul);
+
+    arr.forEach(function(arr){
+        let li = document.createElement('li');
+        let button = document.createElement('button');
+        ul.appendChild(li);
+        li.appendChild(button);
+
+        button.innerHTML += arr; 
+        
+        button.addEventListener('click', adding);
+    })
+
+    // arr.forEach(fruit => console.log(fruit));
+
+    // arr.forEach(fruit => after.innerHTML = fruit);
 
 }
+
+function adding(){
+    console.log('added');
+    
+    let ul = document.createElement('ul');
+    after.appendChild(ul);
+
+    let li = document.createElement('li');
+    let button = document.createElement('button');
+    ul.appendChild(li);
+    li.appendChild(button);
+
+    button.innerHTML += copy; 
+        
+
+    }
+
+
+
+
 
 function subtracting(){
     console.log('subtract');
 }
 
+window.addEventListener('load', displayArr());
+
 add.addEventListener('click', adding);
 subtract.addEventListener('click', subtracting);
+// button.addEventListener('click', () => display.innerHTML = createHex());
 
 
 
