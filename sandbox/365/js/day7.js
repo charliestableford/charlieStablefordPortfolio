a124c3ff2ae0b57ae53754ea6bb329831528657a
Charlie Theodore <charliestableford2@gmail.com>
2020-06-27 17:07:22 -0700

    edit

--- sandbox/365/js/day7.js
+++ sandbox/365/js/day7.js
@@ -1,20 +1,21 @@
 console.log('working');
 
-let hexcode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
 const display = document.querySelector('.display');
 let button = document.querySelector('button');
 // console.log(button);
 
-function createHex(index){
+function createHex(){
     let hex = '#';
+    let hexcode = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
    for(let i =0; i < 6; i++){        
-       console.log(index);
-        let randNum = Math.random() * 9;
+        let randNum = Math.random() * 16;
         let rounded = Math.floor(randNum);
         hex += hexcode[rounded];
     }
 return hex;
 }
 
-button.addEventListener('click', createHex);
+// on click place the outcome of createHex in display.
+button.addEventListener('click', () => display.innerHTML = createHex());
