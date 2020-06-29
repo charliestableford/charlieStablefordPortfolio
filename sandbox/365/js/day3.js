39edaf661b9d49f2b8701004bfc1a9c33b21d1ba
Charlie Theodore <charliestableford2@gmail.com>
2020-06-29 10:51:28 -0700

    working on alarm

--- sandbox/365/js/day3.js
+++ sandbox/365/js/day3.js
@@ -2,25 +2,51 @@ const submit = document.querySelector('.submit');
 const inputHour = document.querySelector('.inputHour');
 const inputMinute = document.querySelector('.inputMinute');
 const alarmSound = document.querySelector('#alarmSound');
+const AmPm = document.querySelector('#AmPm');
 // console.log(alarmSound);
 
-let date = new Date();
-let min = date.getMinutes();
-let hour = date.getHours();
+// let date = new Date();
+// let min = date.getMinutes();
+// let hour = date.getHours();
 
-function alarming(e){
+function play(){
+    alarmSound.play();
+}
+
+setInterval(function alarming(e){
     e.preventDefault();
 
+    let date = new Date();
+    let min = date.getMinutes();
+    let hour = date.getHours();
+    let t = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
     //current hour and min
     console.log(hour, min);
     console.log(inputHour.value, inputMinute.value);
     //if hour and minutes is equal to each other play audio file
      
-    if(inputHour.value === hour && inputMinute.value === min){
+    if(inputHour.value == hour && inputMinute.value == min && AmPm == t){
        console.log(alarmSound);
         //play audio
+        play();
         // return alarmSound.play();
-    }
+    } 
+    timing();
+}, 5000);
+
+//update the timing of the page every second
+function timing(){
+    let date = new Date();
+    let min = date.getMinutes();
+    console.log(min);
+    let hour = date.getHours();
+    console.log(hour);
 }
+//update page every second
+setInterval(timing, 1000);
+
+
 
 submit.addEventListener('click', alarming);
