51c6c2b87e170f8e3ac27ba17ef1b239995d4eca
Charlie Theodore <charliestableford2@gmail.com>
2020-06-30 09:14:31 -0700

    changes to linked

--- sandbox/365/js/day9.js
+++ sandbox/365/js/day9.js
@@ -9,3 +9,26 @@ console.log('day nine');
 //organic eggs $6.79
 // industrial eggs $3.37
 
+function totalLay(){
+    let years = 2;
+    let hours = 22;
+    let daysInYear = 365;
+
+    let hoursInYear = 24 * 365;
+    console.log(hoursInYear);
+
+    // get total for a year times 2 for 2 years.
+    let eggAmount = Math.round(hoursInYear/hours)*2;
+    console.log(eggAmount);
+
+    // total amount of organic eggs over 2 years
+    let OGTotal = Math.round(eggAmount/ 12)*6.79;
+    console.log(`Organic egg total is ${OGTotal}`);
+
+    // total amount of industrial eggs over 2 years
+    let IndustTotal = Math.round(eggAmount/ 12)*3.37;
+
+    console.log(`Industrial egg total is ${IndustTotal}`);
+
+    
+}
