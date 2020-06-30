ee227e7a6fbcef1a4a44d4b7ec621c7535bc15cf
Charlie Theodore <charliestableford2@gmail.com>
2020-06-29 19:41:31 -0700

    eggs

---
+++ sandbox/365/js/day9.js
@@ -0,0 +1,11 @@
+console.log('day nine');
+// a chicken will lay an egg every 22 hours for 2 years
+
+// $30/chicken 
+// feed is $15/month
+// misc $10/month
+//coop - one time cost $500
+
+//organic eggs $6.79
+// industrial eggs $3.37
+
