731155b55a1a40180ecef334a9bd262820102a18
Charlie Theodore <charliestableford2@gmail.com>
2020-07-02 16:44:43 -0700

    day 10

---
+++ sandbox/365/js/day10.js
@@ -0,0 +1,39 @@
+console.log('solve');
+
+const twitterButton = document.querySelector('#js-tweet');
+const newQuoteButton = document.querySelector('#js-new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+  newQuoteButton.disabled = true;
+    // e.preventDefault();
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    displayQuote(json.message);
+    TweetBtn(json.message);
+  } catch {
+    console.log('Call failed');
+  } finally {
+    newQuoteButton.disabled = false;
+  }
+}
+
+function displayQuote(quote) {
+
+// quote = document.querySelector('#quote').value;
+  const quoteText = document.querySelector('#quote');
+  quoteText.innerHTML = quote;
+}
+
+
+function TweetBtn(quote) {
+  twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+}
+
+getQuote();
