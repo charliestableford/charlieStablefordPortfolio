7b7256243d404aa114c3dee80b1a95b66270fac8
Charlie Theodore <charliestableford2@gmail.com>
2020-07-03 14:28:45 -0700

    card update

--- sandbox/365/js/day11.js
+++ sandbox/365/js/day11.js
@@ -1,1 +1,15 @@
 console.log('day 11');
+
+// goal: connect to scrabble dictionary API and unscrabble the word from an input to output a word that is in the dictionary or not
+
+const input = document.querySelector('.input');
+const unscramble = document.querySelector('.unscrambled');
+const submit = document.querySelector('.submit');
+
+submit.addEventListener('click', function(e){
+    e.preventDefault();
+
+    let inputValue = input.value;
+    console.log(inputValue);
+
+});
