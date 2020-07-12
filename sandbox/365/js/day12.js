1a5dae1bed6f66e6bddb7cdc718c9cdeac4ec862
Charlie Theodore <charliestableford2@gmail.com>
2020-07-11 17:23:03 -0700

    progress n calc

--- sandbox/365/js/day12.js
+++ sandbox/365/js/day12.js
@@ -1,1 +1,22 @@
 console.log('in here');
+
+const numOne = document.querySelector('.one');
+const numTwo = document.querySelector('.two');
+const numThree = document.querySelector('.three');
+const numFour = document.querySelector('.four');
+const numFive = document.querySelector('.five');
+const numSix = document.querySelector('.six');
+const numSeven = document.querySelector('.seven');
+const numEight = document.querySelector('.eight');
+const numNine = document.querySelector('.nine');
+const numZero = document.querySelector('.zero');
+
+function displayNum(numOne){
+    //disaply the clicked number in the input field
+    let pressValue = numOne.value;
+    console.log(pressValue);
+    console.log('clicked one');
+}
+
+numOne.addEventListener('click', displayNum);
+
