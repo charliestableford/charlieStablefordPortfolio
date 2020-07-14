3ec1b00e7eb72e74e041360761f112c355f03c15
Charlie Theodore <charliestableford2@gmail.com>
2020-07-13 18:08:26 -0700

    calc

--- sandbox/365/js/day12.js
+++ sandbox/365/js/day12.js
@@ -11,7 +11,7 @@ const numEight = document.querySelector('.eight');
 const numNine = document.querySelector('.nine');
 const numZero = document.querySelector('.zero');
 const plus = document.querySelector('.plus');
-const minus = document.querySelector('.minus');
+let minus = document.querySelector('.minus');
 
 
 
@@ -21,13 +21,19 @@ let input = document.querySelector('.input');
 function handleClick(e){
     //display the clicked number in the input field
     let pressed = e.target.value;
+   
+    console.log(minus);
     
     // console.log(input);
     if (pressed) {
         console.log(`you pressed ${pressed}`);
         // console.log(input);
         input.value = `${pressed}`;
-
+    } 
+    
+    else if(pressed == minus) {
+        let minus = document.querySelector('.minus');
+        console.log(minus);
     }
 
     
