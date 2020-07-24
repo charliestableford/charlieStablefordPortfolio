c4eda928a32b230dc816d14e95f0ed54270b4200
Charlie Theodore <charliestableford2@gmail.com>
2020-07-24 10:47:07 -0700

    adding cards

---
+++ sandbox/365/js/day13.js
@@ -0,0 +1,29 @@
+console.log('made it in here');
+
+async function dribConnect(){
+
+    let user = `charliestableford`;
+    let accesstoken = ``;
+
+    let endpoint = `https://api.dribbble.com/v1/${user}?access_token=OAUTH_TOKEN`;
+}
+
+// async function getWord(e) {
+//     let inputValue = input.value;
+
+//     let endpoint = `https://api.dictionaryapi.dev/api/v1/entries/en/${inputValue}`;
+
+//     e.preventDefualt();
+    
+//     try {
+//       const response = await fetch(endpoint)
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+//       const json = await response.json();
+//       displayQuote(json.message);
+//       console.log(json);
+//     } catch {
+//       console.log('Call failed');
+//     } 
+//   }
