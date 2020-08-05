36f21d9eb8e450f7dabc8d606a8294017168d20c
Charlie Theodore <charliestableford2@gmail.com>
2020-08-05 13:54:00 -0700

    color change

--- src/link.js
+++ src/link.js
@@ -1,30 +1,30 @@
-const triggers = document.querySelectorAll('.toHighLight');
-  const highlight = document.createElement('span');
-  highlight.classList.add('highlight');
-  document.body.appendChild(highlight);
+// const triggers = document.querySelectorAll('.toHighLight');
+//   const highlight = document.createElement('span');
+//   highlight.classList.add('highlight');
+//   document.body.appendChild(highlight);
 
-  //finding the coordinated
-  function highlightLink() {
-      //finding the coordinated
-    const linkCoords = this.getBoundingClientRect();
-    // console.log(linkCoords);
-    const coords = {
-      width: linkCoords.width,
-      height: linkCoords.height,
-      top: linkCoords.top + window.scrollY,
-      left: linkCoords.left + window.scrollX
-    };
+//   //finding the coordinated
+//   function highlightLink() {
+//       //finding the coordinated
+//     const linkCoords = this.getBoundingClientRect();
+//     // console.log(linkCoords);
+//     const coords = {
+//       width: linkCoords.width,
+//       height: linkCoords.height,
+//       top: linkCoords.top + window.scrollY,
+//       left: linkCoords.left + window.scrollX
+//     };
 
-     //switch out reference to linkCoords with the object made above.
-    highlight.style.width = `${coords.width}px`;
-    highlight.style.height = `${coords.height}px`;
-    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+//      //switch out reference to linkCoords with the object made above.
+//     highlight.style.width = `${coords.width}px`;
+//     highlight.style.height = `${coords.height}px`;
+//     highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 
-        // console.log(linkCoords);
-    // console.log(this); // the actual link
-    // console.log('highlight');
-  }
+//         // console.log(linkCoords);
+//     // console.log(this); // the actual link
+//     // console.log('highlight');
+//   }
 
-// forEach trigger run the function highlight when your mouse enters a space defined by above.
-  triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+// // forEach trigger run the function highlight when your mouse enters a space defined by above.
+//   triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
 
--- src/para.js
+++ src/para.js
@@ -1,10 +1,12 @@
 const paraOne = document.querySelector('.abstractSix');
 const paraTwo = document.querySelector('.abstractThree');
 const paraThree = document.querySelector('.abstractFour');
+const paraFour= document.querySelector('.abstractTwo');
 
 window.addEventListener('scroll', function() {
   let scrolled = window.pageYOffset; // Support issues
   paraOne.style.transform = 'translateY(' + (scrolled * 0.5) + 'px)';
   paraTwo.style.transform = 'translateY(' + (scrolled * .5 * -1) + 'px)';
   paraThree.style.transform = 'translateY(' + (scrolled * .5) + 'px)';
+  paraFour.style.transform = 'translateY(' + (scrolled * .5 * 1.5) + 'px)';
 });
