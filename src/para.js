0aaa7aeb6df5093a5a3c5e0f53d817c7afffa752
Charlie Theodore <charliestableford2@gmail.com>
2020-08-08 14:42:44 -0700

    btm

--- src/para.js
+++ src/para.js
@@ -7,6 +7,5 @@ window.addEventListener('scroll', function() {
   let scrolled = window.pageYOffset; // Support issues
   paraOne.style.transform = 'translateY(' + (scrolled * 0.5) + 'px)';
   paraTwo.style.transform = 'translateY(' + (scrolled * .5 * -1) + 'px)';
-  paraThree.style.transform = 'translateY(' + (scrolled * .5) + 'px)';
   paraFour.style.transform = 'translateY(' + (scrolled * .5 * 1.5) + 'px)';
 });
