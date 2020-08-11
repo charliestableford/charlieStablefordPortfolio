36b7f2f65f9d96eef606d4e48eab423a67f16a4d
Charlie Theodore <charliestableford2@gmail.com>
2020-08-10 20:40:50 -0700

    spotify

---
+++ sandbox/365/js/day17.js
@@ -0,0 +1,13 @@
+var client_id = '0e3f58d633e541118f6eb2be87b35789'; 
+var client_secret = '97b691f3d3114a998c94634fb29327f9'; 
+var redirect_uri = 'https://www.charliestableford.com';
+var scopes = 'currently-playing';
+
+
+{
+    "access_token": "BQA6dFPLyVSz7hLWoRUtoEsPWbXM5yDH3tUtZBZexhn9y8qSH1uKVeJAd___NT2ibJl4slD3u2p4QGh5hSYeKWSoEj5BXaX7h_wchX21yFtYciANHewbuqirmo7zw9TNaa_TuK2XIKZPZK6MNGKzXXSy3EhK",
+    "token_type": "Bearer",
+    "expires_in": 3600,
+    "refresh_token": "AQDhToambnrD_hWVJXEwceokFWb8NYtdYdClIhWG5vIEviFfVfTd6qvGoJPxWsd792fdT1UAWwIeQfUTxCeqBgDb-RIRqN7-e3_U2Rc1NHGootbrqpgyyRqLeffsN16aeIk",
+    "scope": ""
+}
