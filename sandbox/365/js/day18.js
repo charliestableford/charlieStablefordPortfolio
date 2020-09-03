f74c38213e3ace6159fd835d2d4f11206aff9c1b
Charlie Theodore <charliestableford2@gmail.com>
2020-09-02 17:20:26 -0700

    finished

--- sandbox/365/js/day18.js
+++ sandbox/365/js/day18.js
@@ -1,28 +1,40 @@
 console.log('download one');
 
-const btn = document.querySelector('.download');
+const btns = document.querySelector('.download');
 console.log('btn');
 
 let counter = 0;
-const items = JSON.parse(localStorage.getItem('items'));
-localStorage.setItem('items', JSON.stringify(counter));
+// const items = JSON.parse(localStorage.getItem('items'));
+
 
 function handleClick(e){
     // console.log(e);
     let counting = counter++;
     console.log(counting); 
 
-    
-        
        
     if(counter > 1){
         alert('Sorry! This is only available for one download!');
-        btn.disabled = true;    
-        
-         localStorage.getItem('items');
-
-       
+        btns.disabled = true;    
+        localStorage.setItem('items', JSON.stringify(counter));
     }
 }
 
-btn.addEventListener('click', handleClick);
+function setTrue(){
+    
+    localStorage.setItem('items',true);
+    
+}
+
+function getClicks(){
+    if (localStorage.getItem('items')){
+        // make the button grey and change text etc. here
+        // also, you said you wanted to disable it?
+        document.querySelector('.download').disabled = 'disabled';
+    }
+    document.querySelector('.download').addEventListener('click', setTrue())
+}
+
+btns.addEventListener('click', handleClick);
+  
+getClicks();
