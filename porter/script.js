b97ef1cf90648808430d444a3450520a60b92699
Charlie Theodore <charliestableford2@gmail.com>
2020-09-15 07:48:45 -0700

    list

--- porter/script.js
+++ porter/script.js
@@ -1,7 +1,22 @@
 console.log('in');
 
 const descript = document.querySelector('.description');
+const list = document.querySelector('.list');
 
-descript.innerHTML = `
-    Hello Porter team! Welcome to Charlie Stableford's redesign challenge. 
+
+function setHTML(){
+    descript.innerHTML = `
+        Hello Porter team! Welcome to Charlie Stableford's redesign challenge. 
+        `;
+    list.innerHTML = `
+    <div>Steps taken during challenge - a quick look</div>
+    <ul>
+    <li>Define challenge</li>
+    <li>Context - What and why is this a problem? Emphasize with the user.</li>
+    <li>Challenge goals - Set goals and microgoals for the challenge. This is done in order to stay on task.</li>
+    <li>Challenge goals - Set goals and microgoals for the challenge. This is done in order to stay on task.</li>
+    </ul>
     `;
+}
+
+document.addEventListener('DOMContentLoaded', setHTML);
