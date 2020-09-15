736a20972f75f25b20f7727a524af731d2f514e4
Charlie Theodore <charliestableford2@gmail.com>
2020-09-15 07:39:04 -0700

    description

--- porter/script.js
+++ porter/script.js
@@ -1,1 +1,7 @@
 console.log('in');
+
+const descript = document.querySelector('.description');
+
+descript.innerHTML = `
+    Hello Porter team! Welcome to Charlie Stableford's redesign challenge. 
+    `;
